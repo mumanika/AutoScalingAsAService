db133dfbf48bd620b05b802b68fb274a12f77c33
Mukul Manikandan <mumanika@HippoMac.local>
2019-11-09 21:05:00 -0500

    changed code to set up each namespace based on the call made by python script.

--- setenv.sh
+++ setenv.sh
@@ -2,34 +2,31 @@
 
 #Set up the namespaces
 
-ns_Input="NS1 NS2"
+ns_Input="NS1 10.10.10.0/24"
+provider_ip=172.16.12.12/24
+provider_nei_ip=172.16.12.13/24
+
+i=$(echo $ns_Input | cut -d ' ' -f 2 | cut -d '.' -f 1-3)
+ip=$(echo $ns_Input | cut -d ' ' -f 1)
 
 
-ip netns del Provider
-ip netns add Provider
-ip link add vProvider type veth peer name v_Provider
-ip link set v_Provider up
-ip addr add 99.99.99.2/24 dev v_Provider
-ip link set vProvider netns Provider
-ip netns exec Provider ip addr add 99.99.99.1/24 dev vProvider
-ip netns exec Provider ip link set vProvider up
-ip route add 99.99.99.0/24 dev v_Provider
+ip netns del $i
+ip netns add $i
+ip netns exec $i brctl addbr $i'_br'
+ip netns exec $i ip link set $i'_br' up
+ip link add v$i type veth peer name 'v_'$i
+ip link set 'v'$i netns $i
+ip link set 'v_'$i netns Provider
+ip netns exec $i ip link set 'v'$i up
+ip netns exec $i ip addr add $ip'.1/24' dev 'v'$i
+ip netns exec Provider ip link set 'v_'$i up 
+ip netns exec Provider ip addr add $ip'.2/24' dev 'v_'$i
+ip netns exec $i ip route add default via $ip'.2/24'
 
-for i in $ns_Input
-do
-	ip netns del $i
-	ip netns add $i
-	ip netns exec $i brctl addbr $i'_br'
-	ip netns exec $i ip link set $i'_br' up
-	ip link add v$i type veth peer name 'v_'$i
-	ip link set 'v'$i netns $i
-	ip link set 'v_'$i netns Provider
-	ip netns exec $i ip link set 'v'$i up
-	ip netns exec $i ip addr add "${i//[!0-9]/}.${i//[!0-9]/}.${i//[!0-9]/}"'.1/24' dev 'v'$i
-	ip netns exec $i ip route add default dev 'v'$i
-	ip netns exec Provider ip link set 'v_'$i up 
-	ip netns exec Provider ip addr add "${i//[!0-9]/}.${i//[!0-9]/}.${i//[!0-9]/}".'2/24' dev 'v_'$i
-done
+
+
+
+
 
 
 
