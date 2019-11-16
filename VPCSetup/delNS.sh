b008693ac2b7ded9ba5e9dda36668f11776064ce
nkovvur <rahul.aettapu@gmail.com>
2019-11-16 01:18:58 +0000

    Scripts to add a veth pair between bridge and VM and delete NS (and removing residual routes in Prov)

---
+++ VPCSetup/delNS.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Desc: Remove the tenant namespace and also delete the routes to the remote IPs from the Provider namespace
+# Args: namespace name, the remote IP on the other site
+
+if [ $# -ne 2 ];
+then
+	exit
+fi
+
+ip netns delete $1
+ip netns exec Provider ip route delete $2
