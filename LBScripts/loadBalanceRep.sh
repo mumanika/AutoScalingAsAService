18fca7f24e733b6305e004de7227c0ab70840cea
nkovvur <rahul.aettapu@gmail.com>
2019-11-17 21:34:23 +0000

    Changed args in LB script

--- LBScripts/loadBalanceRep.sh
+++ LBScripts/loadBalanceRep.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Desc: Replace the rule in the load balancer table. This script is used, when one VM goes down, all the rule entries above that VM must be updated accordingly. That is, the 'every' part of rule should be reduced by 1.
-# Args: Rule number, public destination IP, public port, private destination IP, private port, namespace name, total number of vms for the tenant.
+# Args: Rule number, public destination IP, public port, namespace name, total number of vms for the tenant.
 # teh number of VMS for the tenant has the be the total number before deletion
-if [ $# -ne 7 ]
+if [ $# -ne 5 ]
 then
 	#echo "Illegal number of parameters"
 	exit
@@ -11,21 +11,21 @@ fi
 
 
 
-ruleToDel=$(($7-$1+1))
-every=$(($7-1))
+ruleToDel=$(($5-$1+1))
+every=$(($5-1))
 for (i=1;i<$ruleToDel;i++); do
-	read ev dstip <<< $(ip netns exec $6 iptables -t nat -L PREROUTING $i | awk '{for (I=1;I<=NF;I++) if ($I == "every") {printf "%s %s", $(I+1), $(I+2) };}')
+	read ev dstip <<< $(ip netns exec $4 iptables -t nat -L PREROUTING $i | awk '{for (I=1;I<=NF;I++) if ($I == "every") {printf "%s %s", $(I+1), $(I+2) };}')
 	IFS=':' read -ra prs <<< "$dstip"
 	ipdst=$(echo "$prs[1]")
 	portdst=$(echo "$prs[2]")	
 	if [ $5 -ne 0 ];
 	then
-		ip netns exec $6 iptables -t nat -R PREROUTING $i -p tcp -d $2 --dport $3 -m state --state NEW -m statistic --mode nth --every $every --packet 0 -j DNAT --to-destination $ipdst:$portdst
+		ip netns exec $4 iptables -t nat -R PREROUTING $i -p tcp -d $2 --dport $3 -m state --state NEW -m statistic --mode nth --every $every --packet 0 -j DNAT --to-destination $ipdst:$portdst
 	else
-		ip netns exec $6 iptables -t nat -R PREROUTING $i -p tcp -d $2 --dport $3 -m state --state NEW -m statistic --mode nth --every $every --packet 0 -j DNAT --to-destination $ipdst
+		ip netns exec $4 iptables -t nat -R PREROUTING $i -p tcp -d $2 --dport $3 -m state --state NEW -m statistic --mode nth --every $every --packet 0 -j DNAT --to-destination $ipdst
 	fi
 	every=$(($every-1))
 done
 
-ip netns exec $6 iptables -t nat -D PREROUTING $ruleToDel
-ip netns exec $1 iptables -t nat -Z PREROUTING
+ip netns exec $4 iptables -t nat -D PREROUTING $ruleToDel
+ip netns exec $4 iptables -t nat -Z PREROUTING
