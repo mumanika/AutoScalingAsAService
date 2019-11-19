cfbd22789c00fb97b528ae1efd7b9a0a40ab57d4
nkovvur <rahul.aettapu@gmail.com>
2019-11-19 22:07:13 +0000

    Correction for number of args

--- VPCSetup/delNS.sh
+++ VPCSetup/delNS.sh
@@ -3,15 +3,15 @@
 # Desc: Remove the tenant namespace and also delete the routes to the remote IPs from the Provider namespace
 # Args: namespace name, this sites source address with mask, destination sites address with mask
 
-if [ $# -ne 4 ];
+if [ $# -ne 3 ];
 then
 	exit
 fi
 
 rm -rf /etc/netns/${1}
 ip netns delete $1
-src=$(echo $3 | cut -d '.' -f 1-3)
-dst=$(echo $4 | cut -d '.' -f 1-3)
+src=$(echo $2 | cut -d '.' -f 1-3)
+dst=$(echo $3 | cut -d '.' -f 1-3)
 #rmdir /etc/netns/$1
 ip netns exec Provider iptables -t filter -D FORWARD -i vProv -o Prov$1 -j ACCEPT
 ip netns exec Provider iptables -t filter -D FORWARD -o vProv -i Prov$1 -j ACCEPT
