7582b9e99c21ece16531433505ba3e433f9cfd32
Mukul Manikandan <mmanika@ncsu.edu>
2019-11-20 01:41:03 +0000

    changed code

--- VPCSetup/deleteTenantSubnet.sh
+++ VPCSetup/deleteTenantSubnet.sh
@@ -11,6 +11,16 @@ fi
 ip netns exec $1 ip link set dev ${2} down
 ip netns exec $1 ip link set dev NS${2} down
 ip netns exec $1 ip link set dev ${2}NS down
+
+a=($(brctl show $2 | awk 'NR==2, NR==$NR { print $NF }'))
+
+for i in "${a[@]}"
+do
+        ip netns exec $1 ip link set dev $i down
+        ip netns exec $1 ip link del dev $i
+done
+
+
 ip netns exec $1 brctl delbr ${2}
 ip netns exec $1 ip link del ${2}NS
 lookStr="--interface=NS${2}"
@@ -20,10 +30,3 @@ ip netns exec $1 rm -f /etc/netns/${1}/${3}.hostsfile
 ip netns exec $1 rm -f /etc/${3}.hostsfile 
 
 
-a=($(brctl show $2 | awk 'NR==2, NR==$NR { print $NF }'))
-
-for i in "${a[@]}"
-do
-        ip netns exec $1 ip link set dev $i down
-        ip netns exec $1 ip link del dev $i
-done
