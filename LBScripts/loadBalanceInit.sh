2815ba43ad41ab791ed1892b4d9215e48655a2f5
nkovvur <rahul.aettapu@gmail.com>
2019-11-21 18:01:25 +0000

    Changes

--- LBScripts/loadBalanceInit.sh
+++ LBScripts/loadBalanceInit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Desc: Add iptable rules for load balancing. Add the entry for only new connections. Do this only on the control node's site.
-# Args: Every nth packet, Public Destination IP address, Public IP destination port, the IP address assigned to the loopback interface, namespace name
+# Args: Public Destination IP address, Public IP destination port, the IP address assigned to the loopback interface, namespace name
 
-if [ $# -ne 5 ];
+if [ $# -ne 4 ];
 then
 	echo "Illegal number of params"
 	exit
 fi
 
-ip netns exec $5 iptables -t nat -A PREROUTING  -p tcp -d $2 --dport $3 -m state --state NEW -j DNAT --to-destination $4:$3
+ip netns exec $4 iptables -t nat -A PREROUTING  -p tcp -d $1 --dport $2 -m state --state NEW -j DNAT --to-destination $3:$2
