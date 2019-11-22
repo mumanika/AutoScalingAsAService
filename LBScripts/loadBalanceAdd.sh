7e43d62a6d569746a75e8e6b6855ef0c9486c0e2
Mukul Manikandan <mmanika@ncsu.edu>
2019-11-22 23:21:28 +0000

    Change to iptables script

--- LBScripts/loadBalanceAdd.sh
+++ LBScripts/loadBalanceAdd.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Desc: Add iptable rules for load balancing. Add the entry for only new connections. Only run this on the control node's site because that is where the load balancer is.
-# Args: Every nth packet, IP address assigned to loopback, Public IP destination port, Private destination IP of the guest VM, Private destination port, namespace name
+# Args: Every nth packet, IP address assigned to loopback, Public IP, Public IP destination port, Private destination IP of the guest VM, Private destination port, namespace name
 
-if [ $# -ne 6 ];
+if [ $# -ne 7 ];
 then
 	echo "Illegal number of params"
 	exit
 fi
 
-if [ $5 -ne 0 ];
+if [ $6 -ne 0 ];
 then
-	ip netns exec $6 iptables -t nat -I PREROUTING 1 -p tcp -d $2 --dport $3 -m state --state NEW -m statistic --mode nth --every $1 --packet 0 -j DNAT --to-destination $4:$5
+	ip netns exec $7 iptables -t nat -I PREROUTING 1 -p tcp -d $3 --dport $4 -m state --state NEW -m statistic --mode nth --every $1 --packet 0 -j DNAT --to-destination $5:$6
 else
-	ip netns exec $6 iptables -t nat -I PREROUTING 1 -p tcp -d $2 --dport $3 -m state --state NEW -m statistic --mode nth --every $1 --packet 0 -j DNAT --to-destination $4
+	ip netns exec $7 iptables -t nat -I PREROUTING 1 -p tcp -d $3 --dport $4 -m state --state NEW -m statistic --mode nth --every $1 --packet 0 -j DNAT --to-destination $5
 fi
 
-ip=$(echo $4 | cut -d '.' -f 1-3)
+ip=$(echo $5 | cut -d '.' -f 1-3)
 
-ip netns exec $6 iptables -t nat -I POSTROUTING 1 -p tcp -d $ip.0/24 -j SNAT --to-source $2
+ip netns exec $7 iptables -t nat -I POSTROUTING 1 -p tcp -d $ip.0/24 -j SNAT --to-source $2
