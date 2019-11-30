1da617bf0edd8026c35521ed6c848cbe66b15634
Mukul Manikandan <mmanika@ncsu.edu>
2019-11-30 00:12:28 +0000

    Changes

--- LBScripts/loadBalanceBaseAdd.sh
+++ LBScripts/loadBalanceBaseAdd.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Desc: Add iptable rules for load balancing for the particular LB group on the tenant base namespace. A group is made up of one or more subnet namespaces. (NOTE: make sure to add the route to the loopback interface here at the other site through the GRE tunnel) 
-# Args: Every nth packet, Public IP, Public IP destination port, Private subnet IP exposed to base namespace, Private subnet destination port, namespace name, subnet namespace name
+# Args: Every nth packet, Private subnet IP exposed to base namespace, Private subnet destination port, namespace name, load balancing group name
 
-if [ $# -ne 7 ];
+if [ $# -ne 5 ];
 then
 	echo "Illegal number of params"
 	exit
 fi
 
-if [ $5 -ne 0 ];
+if [ $3 -ne 0 ];
 then
-	ip netns exec $6 iptables -I $7 1 -p tcp -d $2 --dport $3 -m state --state NEW -m statistic --mode nth --every $1 --packet 0 -j DNAT --to-destination $4:$5
+	ip netns exec $4 iptables -t nat -I $5 1 -p tcp -m state --state NEW -m statistic --mode nth --every $1 --packet 0 -j DNAT --to-destination $2:$3
 else
-	ip netns exec $6 iptables -I $7 1 -p tcp -d $2 --dport $3 -m state --state NEW -m statistic --mode nth --every $1 --packet 0 -j DNAT --to-destination $4
+	ip netns exec $4 iptables -t nat -I $5 1 -p tcp -m state --state NEW -m statistic --mode nth --every $1 --packet 0 -j DNAT --to-destination $2
 fi
 
 
--- LBScripts/loadBalanceBaseRep.sh
+++ LBScripts/loadBalanceBaseRep.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Desc: Replace the rule in the load balancer table for the particular group. This script is used, when one subnet is deleted, all the rule entries above that subnet's must be updated accordingly. That is, the 'every' part of rule should be reduced by 1. Make sure to run this on both the sites to update the tables.
-# Args: Rule number for the subnet within the group's chain, public destination IP, public port, base namespace name, total number of subnets in the group, load balancing group name
+# Args: Rule number for the subnet within the group's chain, base namespace name, total number of subnets in the group, load balancing group name
 
-if [ $# -ne 6 ]
+if [ $# -ne 4 ]
 then
 	#echo "Illegal number of parameters"
 	exit
@@ -11,22 +11,22 @@ fi
 
 
 
-ruleToDel=$(($5-$1+1))
-every=$(($5-1))
+ruleToDel=$(($3-$1+1))
+every=$(($3-1))
 for (( i=1;i<$ruleToDel;i++ )); do
-	read ev dstip <<< $(ip netns exec $4 iptables -t nat -L $6 $i | awk '{for (I=1;I<=NF;I++) if ($I == "every") {printf "%s %s", $(I+1), $(I+2) };}')
+	read ev dstip <<< $(ip netns exec $2 iptables -t nat -L $4 $i | awk '{for (I=1;I<=NF;I++) if ($I == "every") {printf "%s %s", $(I+1), $(I+2) };}')
 	IFS=':' read -ra prs <<< "$dstip"
 	ipdst=$(echo "${prs[1]}")
 	portdst=$(echo "${prs[2]}")
 	#echo $prs[2] 	
-	if [ $5 -ne 0 ];
+	if [ $3 -ne 0 ];
 	then
-		ip netns exec $4 iptables -t nat -R $6 $i -p tcp -d $2 --dport $3 -m state --state NEW -m statistic --mode nth --every $every --packet 0 -j DNAT --to-destination $ipdst:$portdst
+		ip netns exec $2 iptables -t nat -R $4 $i -p tcp -m state --state NEW -m statistic --mode nth --every $every --packet 0 -j DNAT --to-destination $ipdst:$portdst
 	else
-		ip netns exec $4 iptables -t nat -R $6 $i -p tcp -d $2 --dport $3 -m state --state NEW -m statistic --mode nth --every $every --packet 0 -j DNAT --to-destination $ipdst
+		ip netns exec $2 iptables -t nat -R $4 $i -p tcp -m state --state NEW -m statistic --mode nth --every $every --packet 0 -j DNAT --to-destination $ipdst
 	fi
 	every=$(($every-1))
 done
 
-ip netns exec $4 iptables -t nat -D $6 $ruleToDel
-ip netns exec $4 iptables -t nat -Z $6
+ip netns exec $2 iptables -t nat -D $4 $ruleToDel
+ip netns exec $2iptables -t nat -Z $4
