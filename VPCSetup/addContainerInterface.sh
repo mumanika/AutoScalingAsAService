c6586eda4b43e6babfd7a68c530c630e9671dccf
Mukul Manikandan <mmanika@ncsu.edu>
2019-12-04 03:37:25 +0000

    Changes to the docker bash script

--- VPCSetup/addContainerInterface.sh
+++ VPCSetup/addContainerInterface.sh
@@ -12,3 +12,8 @@ arg=$(echo ${1}'B')
 ip link set dev ${2}${arg} netns ${3}
 docker container exec --privileged ${2} ip link set dev ${2}${arg} up
 docker container exec --privileged ${2} dhclient ${2}${arg}
+docker container exec --privileged ${2} ip route del default
+read x <<< $(docker container exec ${2} ip -4 addr show ${2}${arg} | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+ip=$(echo $x | cut -d '.' -f 1-3)
+docker container exec --privileged ${2} ip route add default via ${ip}'.1' 
+
