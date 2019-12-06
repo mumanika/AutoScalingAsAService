0b1d498a08ec49f2d3a3a03d176502d3884a0ab7
mmanika@ncsu.edu <mmanika@ncsu.edu>
2019-12-06 22:35:36 +0000

    monitor.py

--- monitor.py
+++ monitor.py
@@ -21,7 +21,19 @@ def update_json(schema):
         json.dump(schema,w_f,indent=4)
 
     for host in mgmt_schema['hosts']:
-         subprocess.call(shlex.split('sudo scp '+file_name+' ' +host['user']+'@'+host['mgmt_ip']+':/home/ece792/AutoScalingAsAService/'))
+        if host['mgmt_ip'] == '172.16.12.13':
+            continue
+        else:
+            subprocess.call(shlex.split('sudo scp '+file_name+' ' +host['user']+'@'+host['mgmt_ip']+':/home/ece792/AutoScalingAsAService/'))
+
+def get_ip(address,last_idx):
+    sub = address.split('.')
+    ip =''
+    for n,p in enumerate(sub):
+        if n != len(sub)-1:
+            ip = ip + p+'.'
+    ip = ip+str(last_idx)
+    return ip
 
 
 
@@ -79,7 +91,7 @@ def dynamic_reactive(grp,schema):
     for grp_meta in schema['scaling_metadata']:
         if grp_meta['name'] == grp_name:
             flag = grp_meta['flag']
-            timer = datetime.datetime.strptime(grp_meta['timer'], '%Y-%m-%d %H:%M:%S.%f');
+            timer = datetime.strptime(grp_meta['timer'], '%Y-%m-%d %H:%M:%S.%f');
             break
 
 
@@ -140,6 +152,7 @@ def main():
     for grp in schema['scaling_groups']:
         #self_heal(grp,schema)
         action = get_action(grp,schema)
+        grp_subnets  = grp[0]['members']
         
         print(action)
         if action == 1:  #scale up
@@ -147,15 +160,70 @@ def main():
             hyp_ip=''
             hyp_user=''
             for hyp_host in mgmt_schema['hosts']:
+                if hyp_host['mgmt_ip'] == '172.16.12.13':
+                    output = subprocess.check_output('/home/ece792/AutoScalingAsAService/hypervisorStats.sh ' ,shell=True).strip()
+                else:
 
-                output = subprocess.check_output('ssh '+hyp_host['user']+'@'+hyp_host['mgmt_ip']+' /home/ece792/AutoScalingAsAService/hypervisorStats.sh ' ,shell=True).strip()
+                    output = subprocess.check_output('ssh '+hyp_host['user']+'@'+hyp_host['mgmt_ip']+'  /home/ece792/AutoScalingAsAService/hypervisorStats.sh ' ,shell=True).strip()
+                #print(output)
                 if float(output) <  min_hyp_use:
                     min_hyp_use = float(output)
                     hyp_ip = hyp_host['mgmt_ip']
                     hyp_user = hyp_host['user']
 
             print(hyp_ip)
-            subprocess.call(shlex.split('ssh '+hyp_user+'@'+hyp_ip+' python  /home/ece792/AutoScalingAsAService/scale_up.py '+file_name+' '+hyp_ip+' ' +grp[0]['name']))
+
+            for host in mgmt_schema['hosts']: #get host_name
+                if host['mgmt_ip'] == hyp_ip:
+                    host_name = host['host']
+
+            subnet_name = ''
+            rand_id = random.choice(grp_subnets)
+
+            for sub in schema['subnets']:
+                flag =1
+                for h in sub:
+                     if h['subnet_id'] ==  rand_id:
+                        subnet_name = h['subnet_name']
+                        flag =0
+                        break
+                 if flag == 0:
+                     break
+
+            print(subnet_name)
+
+            lb_base = False
+            base_ns_subnet =''
+
+            for sub in schema['subnets']:
+                for h in sub:
+                    if h['host'] == host_name and h['subnet_name'] == subnet_name:
+                        if len(h['container_lb_list']) ==0:
+                            lb_base = True
+                        base_ns_subnet = h['base_ns_subnet']
+            
+            base_ns_subnet = get_ip(base_ns_subnet,2)
+            if lb_base == True:
+                n_packet = 0
+                for sub in schema['subnets']:
+                    for h in sub:
+                        if h['subnet_id'] in grp_subnets:
+                            if len(h['container_lb_list']) > 0:
+                                n_packet +=1
+            
+                for hyp_host in mgmt_schema['hosts']:
+                    if hyp_host['mgmt_ip'] == '172.16.12.13':
+                        subprocess.call(shlex.split(' sudo  /home/ece792/AutoScalingAsAService/loadBalanceBaseAdd.sh '+str(n_packet+1)+' '+base_ns_subnet+' 1025  '+schema['ns_name']+ ' ' +grp[0]['name']))
+
+                    else:
+                        subprocess.call(shlex.split('ssh  '+hyp_host['user']+'@'+hyp_host['mgmt_ip']+' sudo  /home/ece792/AutoScalingAsAService/loadBalanceBaseAdd.sh '+str(n_packet+1)+' '+base_ns_subnet+' 1025  '+schema['ns_name']+ ' ' +grp_name))
+
+
+
+            if hyp_ip == '172.16.12.13':
+                subprocess.call(shlex.split(' python  /home/ece792/AutoScalingAsAService/scale_up.py '+file_name+' '+hyp_ip+' ' +grp[0]['name']+' '+subnet_name))
+            else:
+                subprocess.call(shlex.split('ssh '+hyp_user+'@'+hyp_ip+' python  /home/ece792/AutoScalingAsAService/scale_up.py '+file_name+' '+hyp_ip+' ' +grp[0]['name']+' ' +subnet_name))
         
         elif action == 0:
             continue
